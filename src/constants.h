c3023aaf69a008bf40a99b3e9f436849ce76beee
Abhijeet Soman <soman.abhijit@gmail.com>
2022-08-29 23:17:13 -0400

    Added Window and Renderer initialization code

--- src/constants.h
+++ src/constants.h
@@ -1,2 +1,2 @@
-#define FALSE 0
-#define TRUE 1
+#define WINDOW_HEIGHT 600
+#define WINDOW_WIDTH 800
--- src/main.c
+++ src/main.c
@@ -1,14 +1,43 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 #include "./constants.h"
 
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
 int initialize_window(void) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		fprintf(stderr, "Error initializing SDL");
-		return FALSE;
+		fprintf(stderr, "Error initializing SDL.\n");
+		return false;
 	}
 
-	return TRUE;
+	window = SDL_CreateWindow (
+		NULL, 
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		WINDOW_WIDTH,
+		WINDOW_HEIGHT,
+		SDL_WINDOW_BORDERLESS
+	);
+
+    // Don't proceed if window can not be created
+	if (!window) {
+		fprintf(stderr, "Error creating SDL window.\n");
+		return false;
+	}
+
+    
+    // Renderer needs a window to attach
+    renderer = SDL_CreateRenderer(window, -1, 0);
+
+    if (!renderer) {
+    	fprintf(stderr, "Error creating SDL renderer.\n");
+		return false;
+    }
+
+
+	return true;
 }
 
 int main(int argc, char *argv[]) {
