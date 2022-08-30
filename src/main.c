1acacddd1abdf031e011e32532516c8dcb9f89c2
Abhijeet Soman <soman.abhijit@gmail.com>
2022-08-30 09:46:03 -0400

    Added event processor

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <SDL2/SDL.h>
+#include <SDL.h>
 #include "./constants.h"
+
+bool is_game_running = false;
 
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
-int initialize_window(void) {
+bool initialize_window(void) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         fprintf(stderr, "Error initializing SDL.\n");
         return false;
@@ -27,7 +29,6 @@ int initialize_window(void) {
         return false;
     }
 
-
     // Renderer needs a window to attach
     renderer = SDL_CreateRenderer(window, -1, 0);
 
@@ -36,11 +37,50 @@ int initialize_window(void) {
         return false;
     }
 
-
     return true;
 }
 
+void process_input() {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch(event.type) {
+		case SDL_QUIT:
+		    is_game_running = false;
+		    break;
+		case SDL_KEYDOWN:
+		    if (event.key.keysym.sym == SDLK_ESCAPE)
+		    	is_game_running = false;
+		    break;
+	}
+}
+
+void update() {
+
+}
+
+void render() {
+
+}
+
+void destroy_window() {
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
 int main(int argc, char *argv[]) {
-    initialize_window();
+    is_game_running = initialize_window();
+
+
+    /* Game loop is similar to REPL */
+    while(is_game_running) {
+    	process_input();
+    	update();
+    	render();
+    }
+
+    destroy_window();
+
     return 0;
 }
