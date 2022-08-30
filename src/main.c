4f844ad79b9a7e90fe01157bb3aa99dd293378cc
Abhijeet Soman <soman.abhijit@gmail.com>
2022-08-29 23:36:37 -0400

    Fixed indentation

--- src/main.c
+++ src/main.c
@@ -7,40 +7,40 @@ SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
 int initialize_window(void) {
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		fprintf(stderr, "Error initializing SDL.\n");
-		return false;
-	}
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        fprintf(stderr, "Error initializing SDL.\n");
+        return false;
+    }
 
-	window = SDL_CreateWindow (
-		NULL, 
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		WINDOW_WIDTH,
-		WINDOW_HEIGHT,
-		SDL_WINDOW_BORDERLESS
-	);
+    window = SDL_CreateWindow (
+            NULL, 
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            WINDOW_WIDTH,
+            WINDOW_HEIGHT,
+            SDL_WINDOW_BORDERLESS
+            );
 
     // Don't proceed if window can not be created
-	if (!window) {
-		fprintf(stderr, "Error creating SDL window.\n");
-		return false;
-	}
+    if (!window) {
+        fprintf(stderr, "Error creating SDL window.\n");
+        return false;
+    }
 
-    
+
     // Renderer needs a window to attach
     renderer = SDL_CreateRenderer(window, -1, 0);
 
     if (!renderer) {
-    	fprintf(stderr, "Error creating SDL renderer.\n");
-		return false;
+        fprintf(stderr, "Error creating SDL renderer.\n");
+        return false;
     }
 
 
-	return true;
+    return true;
 }
 
 int main(int argc, char *argv[]) {
-	initialize_window();
-	return 0;
+    initialize_window();
+    return 0;
 }
